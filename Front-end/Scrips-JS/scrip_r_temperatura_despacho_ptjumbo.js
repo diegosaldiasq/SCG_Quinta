4026636f89992c3a09fd10eee25c7bcd1636012d
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2023-08-28 17:34:01 -0400

    se crean archivos js faltantes en despacho jumbo y despacho sisa

---
+++ Front-end/Scrips-JS/scrip_r_temperatura_despacho_ptjumbo.js
@@ -0,0 +1,12 @@
+const nombreTecnologo = document.querySelector('#nombre-tecnologo');
+const fechaRegistro = document.querySelector('#fecha-reg');
+
+const cadena = document.querySelector('#cadena');
+const item = document.querySelector('#item');
+const producto = document.querySelector('#producto');
+const temperaturaProducto = document.querySelector('#t-producto');
+const revisionEtiquetado = document.querySelector('#rev-etiquetado');
+const lote = document.querySelector('#lote');
+const fechaVencimiento = document.querySelector('#fecha-vencimiento');
+const accionCorrectiva = document.querySelector('#ac');
+const verificacionAccionCorrectiva = document.querySelector('#vac');
