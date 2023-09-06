288af2df96a85b33f262081f692518715ddecc44
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2023-09-06 13:05:32 -0300

    se corrige errores de js historial termometro

--- Front-end/Scrips-JS/scrips_r_historial_termometro.js
+++ Front-end/Scrips-JS/scrips_r_historial_termometro.js
@@ -7,14 +7,14 @@ const valor2 = document.getElementById('valor2');
 const valor3 = document.getElementById('valor3');
 const valor4 = document.getElementById('valor4');
 const valor5 = document.getElementById('valor5');
-const output1 = document.getElementById('outputx1');
+var output1 = document.getElementById('outputx1');
 
 const valor6 = document.getElementById('valor6');
 const valor7 = document.getElementById('valor7');
 const valor8 = document.getElementById('valor8');
 const valor9 = document.getElementById('valor9');
 const valor10 = document.getElementById('valor10');
-const output2 = document.getElementById('outputx2');
+var output2 = document.getElementById('outputx2');
 
 const factan = document.getElementById('factan');
 const output3 = document.getElementById('outputx3');
@@ -28,34 +28,39 @@ const verificacionAccionCorrectiva = document.getElementById('vac');
 function btnOnClick() {
     const sumaCantidad1 = (Number(valor1.value) + Number(valor2.value) + Number(valor3.value) + Number(valor4.value) + Number(valor5.value))/5;
     output1.innerText = sumaCantidad1.toFixed(1);
+    return output1 = sumaCantidad1;
 }
 
 // funcion para ver promedio de ingreso de termometro patron
 function btnOnClick2() {
     const sumaCantidad2 = (Number(valor6.value) + Number(valor7.value) + Number(valor8.value) + Number(valor9.value) + Number(valor10.value))/5;
     output2.innerText = sumaCantidad2.toFixed(1);
-    return sumaCantidad2;
+    return output2 = sumaCantidad2;
 }
 
 // funcion para ver promedio de ambos termometros, factor de correccion y regla
+var informacionFunction = {};
 function btnOnClick3() {
+    var reglaLog = 'Vacio';
     const promedioCantidad = (((Number(valor1.value) + Number(valor2.value) + Number(valor3.value) + Number(valor4.value) + Number(valor5.value))/5) + ((Number(valor6.value) + Number(valor7.value) + Number(valor8.value) + Number(valor9.value) + Number(valor10.value))/5))/2;
     output3.innerText = promedioCantidad.toFixed(1);
     const x1 = ((Number(valor1.value) + Number(valor2.value) + Number(valor3.value) + Number(valor4.value) + Number(valor5.value))/5) - ((Number(valor6.value) + Number(valor7.value) + Number(valor8.value) + Number(valor9.value) + Number(valor10.value))/5) + Number(factan.value);
     output4.innerText = x1.toFixed(1);
     if (x1 >= -0.5 && x1 <= 0.5) {
         regla.innerText = 'Cumple';
+        reglaLog = 'Cumple';
     } else {
         regla.innerText = 'No cumple';
+        reglaLog = 'No cumple';
     }
 
-    const informacion = {
-        promedioCantidad = promedioCantidad,
-        x1 = x1,
-        regla = regla
+    var informacion = {
+        promedioCantidad: promedioCantidad,
+        x1: x1,
+        regla: reglaLog
     };
 
-    return informacion;
+    return informacionFunction = informacion;
 }
 
 // envio de datos a django
@@ -74,17 +79,17 @@ $(document).ready(function() {
                 valor_3: valor3,
                 valor_4: valor4,
                 valor_5: valor5,
-                promedio_prueba: outputOne,
+                promedio_prueba: output1,
                 valor_6: valor6,
                 valor_7: valor7,
                 valor_8: valor8,
                 valor_9: valor9,
                 valor_10: valor10,
-                promedio_patron: functionTwo,
-                factan: factan,
-                promedio_termometros: functionTree.promedioCantidad,
-                valor_calculo: functionTree.x1,
-                regla: functionTree.regla,
+                promedio_patron: output2,
+                factor_anual: factan,
+                promedio_termometros: informacionFunction.promedioCantidad,
+                nivel_aceptacion: informacionFunction.x1,
+                cumplimiento: informacionFunction.regla,
                 accion_correctiva: accionCorrectiva,
                 verificacion_accion_correctiva: verificacionAccionCorrectiva
             },
@@ -98,3 +103,7 @@ $(document).ready(function() {
         });
     });
 });
+
+//console.log(informacionFunction.promedioCantidad);
+//console.log(informacionFunction.x1);
+//console.log(informacionFunction.regla);
