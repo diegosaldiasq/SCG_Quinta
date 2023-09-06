36fea0d2ea22dd7d114e2b6540f6b6d092909874
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2023-09-06 11:40:17 -0300

    se egragan variables pendientes en js y html de registro historial termometro

--- Front-end/Scrips-JS/scrips_r_historial_termometro.js
+++ Front-end/Scrips-JS/scrips_r_historial_termometro.js
@@ -1,3 +1,7 @@
+const nombreTecnologo = document.getElementById('nombre-tecnologo');
+const fechaRegistro = document.getElementById('fecha-reg');
+
+const codigoTermometro = document.getElementById('codigo-termometro');
 const valor1 = document.getElementById('valor1');
 const valor2 = document.getElementById('valor2');
 const valor3 = document.getElementById('valor3');
@@ -16,17 +20,25 @@ const factan = document.getElementById('factan');
 const output3 = document.getElementById('outputx3');
 const output4 = document.getElementById('outputx4');
 const regla = document.getElementById('regla');
+const accionCorrectiva = document.getElementById('ac');
+const verificacionAccionCorrectiva = document.getElementById('vac');
 
+
+// funcion para ver promedio de ingreso de termometro de muestra
 function btnOnClick() {
     const sumaCantidad1 = (Number(valor1.value) + Number(valor2.value) + Number(valor3.value) + Number(valor4.value) + Number(valor5.value))/5;
     output1.innerText = sumaCantidad1.toFixed(1);
+    return sumaCantidad1;
 }
 
+// funcion para ver promedio de ingreso de termometro patron
 function btnOnClick2() {
     const sumaCantidad2 = (Number(valor6.value) + Number(valor7.value) + Number(valor8.value) + Number(valor9.value) + Number(valor10.value))/5;
     output2.innerText = sumaCantidad2.toFixed(1);
+    return sumaCantidad2;
 }
 
+// funcion para ver promedio de ambos termometros, factor de correccion y regla
 function btnOnClick3() {
     const promedioCantidad = (((Number(valor1.value) + Number(valor2.value) + Number(valor3.value) + Number(valor4.value) + Number(valor5.value))/5) + ((Number(valor6.value) + Number(valor7.value) + Number(valor8.value) + Number(valor9.value) + Number(valor10.value))/5))/2;
     output3.innerText = promedioCantidad.toFixed(1);
@@ -37,4 +49,41 @@ function btnOnClick3() {
     } else {
         regla.innerText = 'No cumple';
     }
+
+    const informacion = {
+        promedioCantidad = promedioCantidad,
+        x1 = x1,
+        regla = regla
+    };
+
+    return informacion;
 }
+
+// envio de datos a django
+$(document).ready(function() {
+    $("#miBoton").click(function() {
+
+        $.ajax({
+            url: "../../Back-end/SCG_Quinta/...[].../views.py",  // Ruta a tu vista Django
+            method: "POST",
+            data: {
+                nombre_tecnologo: nombreTecnologo,
+                fecha_registro: fechaRegistro,
+                lote: lote,
+                turno: turno,
+                tipo_metal: tipoMetal,
+                medicion: medicion,
+                producto: producto,
+                observaciones: observaciones,
+                accion_correctiva: accionCorrectiva
+            },
+            success: function(response) {
+                // Hacer algo con la respuesta del servidor
+                console.log(response);
+            },
+            error: function() {
+                // Manejar errores
+            }
+        });
+    });
+});
