6eaf03aee939b700ce83c2a2aa275b6962da42c2
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2023-10-05 15:55:22 -0300

    se arregla error de js de transision, sin resultado probado, pero cerca

--- Back-end/SCG_Quinta/login/static/login/scrips_crear_cuenta.js
+++ Back-end/SCG_Quinta/login/static/login/scrips_crear_cuenta.js
@@ -44,8 +44,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             });
         }); 
+        setTimeout(function() {
+            window.location.href = "/login/cuenta_creada/";
+        }, 8000);  // 8000 milisegundos son 8 segundos
     });
-    setTimeout(function() {
-        window.location.href = "/login/cuenta_creada/";
-    }, 8000);  // 8000 milisegundos son 8 segundos
 });
