7e5ccae2f84418075088f5883c0dbaf2238b4124
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2023-10-11 11:23:14 -0300

    se intenta solucionar transiciones de validacion de password para recuperacion, sin probar

--- Back-end/SCG_Quinta/login/static/login/scrips_ingresa_rut.js
+++ Back-end/SCG_Quinta/login/static/login/scrips_ingresa_rut.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('miBoton').addEventListener('click', function() {
-        const rutEnWeb = document.getElementById('rut').value;
-        fetch('/login/vista_ingresa_rut/') // Ruta a tu vista Django
-            .then(response => response.json())
-            .then(data => {
-                const rutEnBase = data.map(item => item.rut_base);
-                console.log(rutEnBase, rutEnWeb);
-                if (rutEnBase.includes(rutEnWeb)) {
-                    //window.location.href = "/login/pasword/";
-                    alert("El rut ingresado se encuentra en la base de datos");
-                    return true;
-                } else {
-                    alert("El rut ingresado no se encuentra en la base de datos");
-                    return false;
-                }
-            })
-            .catch(error => console.error('Error:', error));
+    document.getElementById('miBoton').addEventListener('click', async function() {
+        var rutEnWeb = document.getElementById('rut').value;
+        var response = await fetch('/login/vista_ingresa_rut/') // Ruta a tu vista Django
+        var data = await response.json();    
+        var rutEnBase = await data.map(item => item.rut_base);
+        console.log(rutEnBase, rutEnWeb);
+        if (rutEnBase.includes(rutEnWeb)) {
+            //window.location.href = "/login/pasword/";
+            alert("El rut ingresado se encuentra en la base de datos");
+            return true;
+        } else {
+            alert("El rut ingresado no se encuentra en la base de datos");
+            return false;
+        }   
     });
 });
