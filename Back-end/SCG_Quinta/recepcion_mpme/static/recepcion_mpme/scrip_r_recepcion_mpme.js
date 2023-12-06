1e866a72391763153d14b3ad7768e06ee5554955
Diego Saldias <diego.saldias.quijada@gmail.com>
2023-12-06 00:13:36 -0300

    se crea alternativa para generar respuesta del servidor en el template por js en recepcion mpme

--- Back-end/SCG_Quinta/recepcion_mpme/static/recepcion_mpme/scrip_r_recepcion_mpme.js
+++ Back-end/SCG_Quinta/recepcion_mpme/static/recepcion_mpme/scrip_r_recepcion_mpme.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* document.addEventListener("DOMContentLoaded", function() {
     // Tu código JavaScript aquí
     var loteDia = document.getElementById('lote-dia');
     var nombreProveedor = document.getElementById('proveedor');
@@ -76,8 +76,8 @@ document.addEventListener("DOMContentLoaded", function() {
                     grados_brix: gradosBrix
                 },
                 success: function(response) {
-                    // Hacer algo con la respuesta del servidor
-                    console.log(response);
+                    //Hacer algo con la respuesta del servidor
+                    //console.log(response);
                     var mensaje;
                     if (response.mensaje === 'Datos guardados exitosamente') {
                         mensaje = response.mensaje;
@@ -92,5 +92,81 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             });
         });
+    });
+}); */
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("miBoton").addEventListener("click", async function() {
+        try {
+            event.preventDefault(); // <-- para no recargar la pagina al enviar el formulario
+            var loteDia = $("#lote-dia").val();
+            var nombreProveedor = $("#proveedor").val();
+            var nombreProducto = $("#producto").val();
+            var fechaDeElaboracion = $("#fecha-elab").val();
+            var fechaDeVencimiento = $("#fecha-venc").val();
+            var loteProducto = $("#lote-producto").val();
+            var numeroFactura = $("#numero-factura").val();
+            var higiene = $("#higiene").val();
+            var rs = $("#rs").val();
+            var temperaturaTransporte = $("#temperatura-trans").val();
+            var apariencia = $("#apariencia").val();
+            var textura = $("#textura").val();
+            var ausenciaMaterialExtraño = $("#ausencia-me").val();
+            var temperaturaProducto = $("#temperatura-prod").val();
+            var condicionEnvase = $("#condicion-env").val();
+            var color = $("#color").val();
+            var olor = $("#olor").val();
+            var sabor = $("#sabor").val();
+            var gradosBrix = $("#grados-brix").val();
+
+            // agregar valores a datos
+
+            var datos = {
+                lote_dia: loteDia,
+                nombre_proveedor: nombreProveedor,
+                nombre_producto: nombreProducto,
+                fecha_elaboracion: fechaDeElaboracion,
+                fecha_vencimiento: fechaDeVencimiento,
+                lote_producto: loteProducto,
+                numero_factura: numeroFactura,
+                higiene: higiene,
+                rs: rs,
+                temperatura_transporte: temperaturaTransporte,
+                apariencia: apariencia,
+                textura: textura,
+                ausencia_material_extraño: ausenciaMaterialExtraño,
+                temperatura_producto: temperaturaProducto,
+                condicion_envase: condicionEnvase,
+                color: color,
+                olor: olor,
+                sabor: sabor,
+                grados_brix: gradosBrix
+            }
+            
+            // Obtener el CSRF token
+            var csrfToken = document.querySelector("[name=csrfmiddlewaretoken]").value;
+
+            var response = await fetch('/recepcion_mpme/vista_recepcion_mpme/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken // Aquí deberías agregar el csrf token para Django si es necesario
+                },
+                body: JSON.stringify({ dato: datos })
+            });
+            var data = await response.json();
+            debugger; // <-- Agrega esta línea
+            if (data.existe) {
+                alert("Datos guardados exitosamente!!");
+                location.reload();
+                return true;
+            } else {
+                alert("No se pudo guardar... revisa nuevamente!!");
+                return false;
+            }
+        } catch (error) {
+            console.error("Hubo un error:", error);
+            alert("Hubo un problema al cargar los datos, formatos no coinciden!!");
+        }
     });
 });
--- Back-end/SCG_Quinta/recepcion_mpme/views.py
+++ Back-end/SCG_Quinta/recepcion_mpme/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.utils import timezone
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+import json
 
 # Create your views here.
 
@@ -16,55 +17,60 @@ def recepcion_mpme(request):
 @login_required
 def vista_recepcion_mpme(request):
     if request.method == 'POST':
-        nombre_tecnologo = request.user.nombre_completo
-        lote_dia = request.POST.get('lote_dia')
-        fecha_registro = timezone.now()
-        nombre_proveedor = request.POST.get('nombre_proveedor')
-        nombre_producto = request.POST.get('nombre_producto')
-        fecha_elaboracion = timezone.make_aware(datetime.strptime(request.POST.get('fecha_elaboracion'), '%Y-%m-%d'), timezone=timezone.utc)
-        fecha_vencimiento = timezone.make_aware(datetime.strptime(request.POST.get('fecha_vencimiento'), '%Y-%m-%d'), timezone=timezone.utc)
-        lote_producto = request.POST.get('lote_producto')
-        numero_factura = request.POST.get('numero_factura')
-        higiene = request.POST.get('higiene')
-        rs = request.POST.get('rs')
-        temperatura_transporte = request.POST.get('temperatura_transporte')
-        apariencia = request.POST.get('apariencia')
-        textura = request.POST.get('textura')
-        ausencia_material_extraño = request.POST.get('ausencia_material_extraño')
-        temperatura_producto = request.POST.get('temperatura_producto')
-        condicion_envase = request.POST.get('condicion_envase')
-        color = request.POST.get('color')
-        olor = request.POST.get('olor')
-        sabor = request.POST.get('sabor')
-        grados_brix = request.POST.get('grados_brix')
+        data = json.loads(request.body.decode('utf-8'))
+        dato = data.get('dato', None)
+        if dato:
+            nombre_tecnologo = request.user.nombre_completo
+            lote_dia = dato.get('lote_dia')
+            fecha_registro = timezone.now()
+            nombre_proveedor = dato.get('nombre_proveedor')
+            nombre_producto = dato.get('nombre_producto')
+            fecha_elaboracion = timezone.make_aware(datetime.strptime(dato.get('fecha_elaboracion'), '%Y-%m-%d'), timezone=timezone.utc)
+            fecha_vencimiento = timezone.make_aware(datetime.strptime(dato.get('fecha_vencimiento'), '%Y-%m-%d'), timezone=timezone.utc)
+            lote_producto = dato.get('lote_producto')
+            numero_factura = dato.get('numero_factura')
+            higiene = dato.get('higiene')
+            rs = dato.get('rs')
+            temperatura_transporte = dato.get('temperatura_transporte')
+            apariencia = dato.get('apariencia')
+            textura = dato.get('textura')
+            ausencia_material_extraño = dato.get('ausencia_material_extraño')
+            temperatura_producto = dato.get('temperatura_producto')
+            condicion_envase = dato.get('condicion_envase')
+            color = dato.get('color')
+            olor = dato.get('olor')
+            sabor = dato.get('sabor')
+            grados_brix = dato.get('grados_brix')
 
+            datos = DatosFormularioRecepcionMpMe(
+                nombre_tecnologo=nombre_tecnologo,
+                lote_dia=lote_dia, 
+                fecha_registro=fecha_registro,
+                nombre_proveedor=nombre_proveedor,
+                nombre_producto=nombre_producto,
+                fecha_elaboracion=fecha_elaboracion,
+                fecha_vencimiento=fecha_vencimiento,
+                lote_producto=lote_producto,
+                numero_factura=numero_factura,
+                higiene=higiene,
+                rs=rs,
+                temperatura_transporte=temperatura_transporte,
+                apariencia=apariencia,
+                textura=textura,
+                ausencia_material_extraño=ausencia_material_extraño,
+                temperatura_producto=temperatura_producto,
+                condicion_envase=condicion_envase,
+                color=color,
+                olor=olor,
+                sabor=sabor,
+                grados_brix=grados_brix
+                )
+            datos.save()
 
-        datos = DatosFormularioRecepcionMpMe(
-            nombre_tecnologo=nombre_tecnologo,
-            lote_dia=lote_dia, 
-            fecha_registro=fecha_registro,
-            nombre_proveedor=nombre_proveedor,
-            nombre_producto=nombre_producto,
-            fecha_elaboracion=fecha_elaboracion,
-            fecha_vencimiento=fecha_vencimiento,
-            lote_producto=lote_producto,
-            numero_factura=numero_factura,
-            higiene=higiene,
-            rs=rs,
-            temperatura_transporte=temperatura_transporte,
-            apariencia=apariencia,
-            textura=textura,
-            ausencia_material_extraño=ausencia_material_extraño,
-            temperatura_producto=temperatura_producto,
-            condicion_envase=condicion_envase,
-            color=color,
-            olor=olor,
-            sabor=sabor,
-            grados_brix=grados_brix
-            )
-        datos.save()
+            return JsonResponse({'existe': True})
+        else:
+            return JsonResponse({'existe': False})
 
-        return JsonResponse({'mensaje': 'Datos guardados exitosamente'})
 
 @login_required
 def redireccionar_selecciones(request):
