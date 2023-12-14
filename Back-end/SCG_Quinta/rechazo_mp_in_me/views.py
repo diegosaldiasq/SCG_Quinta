ab93748388aed925250f2278923b970b97fa7360
Diego Saldias <diego.saldias.quijada@gmail.com>
2023-12-13 23:08:30 -0300

    se crea aseguramiento para rechazo mp in me

--- Back-end/SCG_Quinta/rechazo_mp_in_me/views.py
+++ Back-end/SCG_Quinta/rechazo_mp_in_me/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.utils import timezone
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+import json
 
 # Create your views here.
 
@@ -16,39 +17,44 @@ def rechazo_mp_in_me(request):
 @login_required
 def vista_rechazo_mp_in_me(request):
     if request.method == 'POST':
-        nombre_tecnologo = request.user.nombre_completo
-        fecha_registro = timezone.now()
-        nombre_proveedor = request.POST.get('nombre_proveedor')
-        numero_factura = request.POST.get('numero_factura')
-        nombre_transportista = request.POST.get('nombre_transportista')
-        nombre_producto = request.POST.get('nombre_producto')
-        fecha_elaboracion = timezone.make_aware(datetime.strptime(request.POST.get('fecha_elaboracion'), '%Y-%m-%d'), timezone=timezone.utc)
-        lote = request.POST.get('lote')
-        fecha_vencimiento = timezone.make_aware(datetime.strptime(request.POST.get('fecha_vencimiento'), '%Y-%m-%d'), timezone=timezone.utc)
-        motivo_rechazo = request.POST.get('motivo_rechazo')
-        cantidad_producto_involucrado = request.POST.get('cantidad_producto_involucrado')
-        unidad_de_medida = request.POST.get('unidad_de_medida')
-        clasificacion = request.POST.get('clasificacion')
+        data = json.loads(request.body.decode('utf-8'))
+        dato = data.get('dato', None)
+        if dato:
+            nombre_tecnologo = request.user.nombre_completo
+            fecha_registro = timezone.now()
+            nombre_proveedor = request.POST.get('nombre_proveedor')
+            numero_factura = request.POST.get('numero_factura')
+            nombre_transportista = request.POST.get('nombre_transportista')
+            nombre_producto = request.POST.get('nombre_producto')
+            fecha_elaboracion = timezone.make_aware(datetime.strptime(request.POST.get('fecha_elaboracion'), '%Y-%m-%d'), timezone=timezone.utc)
+            lote = request.POST.get('lote')
+            fecha_vencimiento = timezone.make_aware(datetime.strptime(request.POST.get('fecha_vencimiento'), '%Y-%m-%d'), timezone=timezone.utc)
+            motivo_rechazo = request.POST.get('motivo_rechazo')
+            cantidad_producto_involucrado = request.POST.get('cantidad_producto_involucrado')
+            unidad_de_medida = request.POST.get('unidad_de_medida')
+            clasificacion = request.POST.get('clasificacion')
 
 
-        datos = DatosFormularioRechazoMpInMe(
-            nombre_tecnologo=nombre_tecnologo,
-            fecha_registro=fecha_registro,
-            nombre_proveedor=nombre_proveedor,
-            numero_factura=numero_factura,
-            nombre_transportista=nombre_transportista,
-            nombre_producto=nombre_producto,
-            fecha_elaboracion=fecha_elaboracion,
-            lote=lote,
-            fecha_vencimiento=fecha_vencimiento,
-            motivo_rechazo=motivo_rechazo,
-            cantidad_producto_involucrado=cantidad_producto_involucrado,
-            unidad_de_medida=unidad_de_medida,
-            clasificacion=clasificacion
-            )
-        datos.save()
+            datos = DatosFormularioRechazoMpInMe(
+                nombre_tecnologo=nombre_tecnologo,
+                fecha_registro=fecha_registro,
+                nombre_proveedor=nombre_proveedor,
+                numero_factura=numero_factura,
+                nombre_transportista=nombre_transportista,
+                nombre_producto=nombre_producto,
+                fecha_elaboracion=fecha_elaboracion,
+                lote=lote,
+                fecha_vencimiento=fecha_vencimiento,
+                motivo_rechazo=motivo_rechazo,
+                cantidad_producto_involucrado=cantidad_producto_involucrado,
+                unidad_de_medida=unidad_de_medida,
+                clasificacion=clasificacion
+                )
+            datos.save()
 
-        return JsonResponse({'mensaje': 'Datos guardados exitosamente'})
+            return JsonResponse({'existe': True})
+        else:
+            return JsonResponse({'existe': False})
 
 @login_required
 def redireccionar_selecciones(request):
--- Back-end/SCG_Quinta/staticfiles/rechazo_mp_in_me/scrip_r_informe_rechazo_mp_in_me.js
+++ Back-end/SCG_Quinta/staticfiles/rechazo_mp_in_me/scrip_r_informe_rechazo_mp_in_me.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     // Tu código JavaScript aquí
     var nombreDeProveedor = document.getElementById('nomb-prov');
     var numeroFactura = document.getElementById('numero-factura');
@@ -64,5 +64,64 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             });
         });
+    });
+});*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("miBoton").addEventListener("click", async function() {
+        try {
+            event.preventDefault(); // <-- para no recargar la pagina al enviar el formulario
+            var nombreDeProveedor = $("#nomb-prov").val();
+            var numeroFactura = $("#numero-factura").val();
+            var nombreDelTransportista = $("#nomb-trans").val();
+            var nombreProducto = $("#nombre-producto").val();
+            var fechaElaboracion = $("#fecha-elab").val();
+            var lote = $("#lote").val();
+            var fechaVencimiento = $("#fecha-venc").val();
+            var motivoRechazo = $("#motivo-rechazo").val();
+            var cantidadProductoInvolucrado = $("#cantidad-producto").val();
+            var unidadDeMedida = $("#udm").val();
+            var clasificacion = $("#clasificacion").val();
+
+            // agregar valores a datos
+
+            var datos = {
+                nombre_proveedor: nombreDeProveedor,
+                numero_factura: numeroFactura,
+                nombre_transportista: nombreDelTransportista,
+                nombre_producto: nombreProducto,
+                fecha_elaboracion: fechaElaboracion,
+                lote: lote,
+                fecha_vencimiento: fechaVencimiento,
+                motivo_rechazo: motivoRechazo,
+                cantidad_producto_involucrado: cantidadProductoInvolucrado,
+                unidad_de_medida: unidadDeMedida,
+                clasificacion: clasificacion
+            }
+            
+            // Obtener el CSRF token
+            var csrfToken = document.querySelector("[name=csrfmiddlewaretoken]").value;
+
+            var response = await fetch('/rechazo_mp_in_me/vista_rechazo_mp_in_me/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken // Aquí deberías agregar el csrf token para Django si es necesario
+                },
+                body: JSON.stringify({ dato: datos })
+            });
+            var data = await response.json();
+            debugger; // <-- Agrega esta línea
+            if (data.existe) {
+                alert("Datos guardados exitosamente!!");
+                location.reload();
+            } else {
+                alert("No se pudo guardar... revisa nuevamente!!");
+                return false;
+            }
+        } catch (error) {
+            console.error("Hubo un error:", error);
+            alert("Hubo un problema al cargar los datos, formatos no coinciden!!");
+        }
     });
 });
