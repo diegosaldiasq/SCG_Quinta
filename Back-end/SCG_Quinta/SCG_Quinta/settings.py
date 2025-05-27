7fb008e006f87e90667ae7a416909cb7d39ed7f5
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2025-05-27 16:37:25 -0400

    se agrega direccion publica para prueba en instancia EC2 AWS

--- Back-end/SCG_Quinta/SCG_Quinta/settings.py
+++ Back-end/SCG_Quinta/SCG_Quinta/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--^2f**zyj9i4&tn2@ytw(rk7si&z0qum#($#4c87a3o$^1t*5r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','54.167.27.148']
 
 
 # Application definition
