21783bc429ac74fdef80cdf92ab059de9419fa20
Diego Saldias <diego.saldias.quijada@gmail.com>
2025-06-02 20:32:56 -0400

    se configura crear cuenta.js

--- Back-end/SCG_Quinta/staticfiles/login/scrips_crear_cuenta.js
+++ Back-end/SCG_Quinta/staticfiles/login/scrips_crear_cuenta.js
@@ -60,6 +60,59 @@
 //});
 
 // Envio de datos a Django
+//$.ajaxSetup({
+//    beforeSend: function(xhr, settings) {
+//        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+//            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+//        }
+//    }
+//});
+
+//document.addEventListener("DOMContentLoaded", function() {
+//    $(document).ready(function() {
+//        $("#miBoton").click(function() {
+//            var nombreCompleto = $("#nombre-completo").val();
+//            var perfilUsuario = $("#perfil-usuario").val();
+//            var rut = $("#rut").val();
+//            var password = $("#password").val();
+//            var newPassword = $("#new-password").val();
+//
+//            if (password != newPassword) {  // Si las contraseñas no coinciden  
+//                alert("Las contraseñas no coinciden");
+//                return false;
+//            }
+//    
+//            $.ajax({
+//                url: "/login/vista_crear_cuenta/",  // Ruta a tu vista Django
+//                method: "POST",
+//                data: {
+//                    nombre_completo: nombreCompleto,
+//                    perfil_usuario: perfilUsuario,
+//                    rut: rut,
+//                    password: password,
+//                    new_password: newPassword
+//                },
+//                success: function(response) {
+//                    // Hacer algo con la respuesta del servidor
+//                    console.log(response);
+//                    $("#mensaje").text(response.mensaje);
+//                    window.location.href = "/login/cuenta_creada/";
+//                },
+//                error: function(response) {
+//                    // Manejar errores
+//                    console.error("Error en la solicitud AJAX:", response);
+//                    alert("Error al crear la cuenta. Por favor, inténtalo de nuevo.");
+//                },
+//                error: function() {
+//                    // Manejar errores
+//                    console.error("Error en la solicitud AJAX:", textStatus, errorThrown);
+//                }
+//            });
+//        }); 
+//    });
+//});
+
+// Configuración global de jQuery para incluir CSRF en todas las peticiones POST/AJAX
 $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
@@ -69,45 +122,47 @@ $.ajaxSetup({
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    $(document).ready(function() {
-        $("#miBoton").click(function() {
+    document.getElementById("miBoton").addEventListener("click", async function() {
+        try {
+            event.preventDefault(); // <-- para no recargar la pagina al enviar el formulario
             var nombreCompleto = $("#nombre-completo").val();
             var perfilUsuario = $("#perfil-usuario").val();
             var rut = $("#rut").val();
             var password = $("#password").val();
             var newPassword = $("#new-password").val();
 
-            if (password != newPassword) {  // Si las contraseñas no coinciden  
-                alert("Las contraseñas no coinciden");
+            // agregar valores a datos
+
+            var datos = {
+                nombre_completo: nombreCompleto,
+                perfil_usuario: perfilUsuario,
+                rut: rut,
+                password: password,
+                new_password: newPassword
+            }
+            
+            // Obtener el CSRF token
+            var csrfToken = document.querySelector("[name=csrfmiddlewaretoken]").value;
+
+            var response = await fetch('/login/vista_crear_cuenta/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken // Aquí deberías agregar el csrf token para Django si es necesario
+                },
+                body: JSON.stringify({ dato: datos })
+            });
+            var data = await response.json();
+            debugger; // <-- Agrega esta línea
+            if (data.existe) {
+                window.location.href = "/login/cuenta_creada/";
+            } else {
+                alert("No se pudo guardar... revisa nuevamente!!");
                 return false;
             }
-    
-            $.ajax({
-                url: "/login/vista_crear_cuenta/",  // Ruta a tu vista Django
-                method: "POST",
-                data: {
-                    nombre_completo: nombreCompleto,
-                    perfil_usuario: perfilUsuario,
-                    rut: rut,
-                    password: password,
-                    new_password: newPassword
-                },
-                success: function(response) {
-                    // Hacer algo con la respuesta del servidor
-                    console.log(response);
-                    $("#mensaje").text(response.mensaje);
-                    window.location.href = "/login/cuenta_creada/";
-                },
-                error: function(response) {
-                    // Manejar errores
-                    console.error("Error en la solicitud AJAX:", response);
-                    alert("Error al crear la cuenta. Por favor, inténtalo de nuevo.");
-                },
-                error: function() {
-                    // Manejar errores
-                    console.error("Error en la solicitud AJAX:", textStatus, errorThrown);
-                }
-            });
-        }); 
+        } catch (error) {
+            console.error("Hubo un error:", error);
+            alert("Hubo un problema al cargar los datos, formatos no coinciden!!");
+        }
     });
 });
