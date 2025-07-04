969cd455d4eb3365f32f02f188f7fd4af8549ee0
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2025-07-04 16:06:04 -0400

    se corrigen rutas de urls calculo oee

--- Back-end/SCG_Quinta/calculo_oee/urls.py
+++ Back-end/SCG_Quinta/calculo_oee/urls.py
@@ -6,10 +6,11 @@ from . import views
 
 urlpatterns = [
     #path("", views.calculo_oee, name="calculo_oee"),
+    path('crear_turno/', views.crear_turno, name='crear_turno'),
     path('turno-exito/', lambda request: HttpResponse("Turno registrado con éxito."), name='turno_exito'),
     path('resumen/<int:turno_id>/', views.resumen_turno, name='resumen_turno'),
-    path('cerrar-turno/<int:turno_id>/', views.cerrar_turno, name='cerrar_turno'),
-    path('turnos/', views.lista_turnos, name='lista_turnos'),
+    path('cerrar_turno/<int:turno_id>/', views.cerrar_turno, name='cerrar_turno'),
+    path('lista_turnos/', views.lista_turnos, name='lista_turnos'),
     path('detalle-turno/<int:turno_id>/', views.detalle_turno, name='detalle_turno'),
     path('marcar-verificado/<int:turno_id>/', views.marcar_verificado, name='marcar_verificado')
 ]
