b320874504e06abc734b87b5c8976cdc94201e9c
Diego Saldias <diego.saldias.quijada@gmail.com>
2025-07-06 13:00:42 -0400

    se agregan formatos a formulario

--- Back-end/SCG_Quinta/calculo_oee/forms.py
+++ Back-end/SCG_Quinta/calculo_oee/forms.py
@@ -1,7 +1,37 @@
 from django import forms
 from .models import TurnoOEE
 
+# Definición de opciones para los campos de selección
+CLIENTES = [
+    ("Cliente A", "Cliente A"),
+    ("Cliente B", "Cliente B"),
+    ("Cliente C", "Cliente C"),
+]
+
+CODIGOS = [
+    ("COD001", "COD001"),
+    ("COD002", "COD002"),
+    ("COD003", "COD003"),
+]
+
+PRODUCTOS = [
+    ("Producto X", "Producto X"),
+    ("Producto Y", "Producto Y"),
+    ("Producto Z", "Producto Z"),
+]
+
 class TurnoOEEForm(forms.ModelForm):
+    cliente = forms.ChoiceField(choices=CLIENTES, widget=forms.Select(attrs={'class': 'input'}))
+    codigo = forms.ChoiceField(choices=CODIGOS, widget=forms.Select(attrs={'class': 'input'}))
+    producto = forms.ChoiceField(choices=PRODUCTOS, widget=forms.Select(attrs={'class': 'input'}))
+    fecha = forms.DateField(
+        input_formats=['%d-%m-%Y'],
+        widget=forms.TextInput(attrs={
+            'placeholder': 'DD-MM-YYYY',
+            'pattern': '\\d{2}-\\d{2}-\\d{4}',  # Validación HTML
+            'class': 'input'
+        })
+    )
     class Meta:
         model = TurnoOEE
         fields = ['fecha', 'cliente', 'codigo', 'producto', 'linea', 'turno', 'hora_inicio', 'hora_fin', 'tiempo_planeado', 'produccion_planeada']
