f8ce582ab521da35b199354dd0edad239072c33c
Diego Saldias <diego.saldias.quijada@gmail.com>
2025-07-06 20:06:36 -0400

    se crean flujos automaticos para cliente codigo y producto en crear turno calculo oee

--- Back-end/SCG_Quinta/calculo_oee/forms.py
+++ Back-end/SCG_Quinta/calculo_oee/forms.py
@@ -11,6 +11,26 @@ class TurnoOEEForm(forms.ModelForm):
         input_formats=['%d-%m-%Y'],  # Formato de entrada para el campo de fecha
         help_text="Fecha del turno (formato: DD-MM-YYYY)"
     )
+    CLIENTE_CHOICES = [
+        ('Cliente A', 'Cliente A'),
+        ('Cliente B', 'Cliente B'),
+        ('Cliente C', 'Cliente C'),
+    ]
+    cliente = forms.ChoiceField(
+        choices=CLIENTE_CHOICES,
+        widget=forms.Select(attrs={'id': 'id_cliente'})
+    )
+    producto = forms.ChoiceField(
+        choices=[('', '--Seleccionar cliente primero--')],
+        widget=forms.Select(attrs={'id': 'id_producto'})
+    )
+    codigo = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'readonly': 'readonly',
+            'id': 'id_codigo',
+            'placeholder': 'Se autocompleta'
+        })
+    )
     class Meta:
         model = TurnoOEE
         fields = ['fecha', 'cliente', 'producto', 'codigo', 'linea', 'turno', 'hora_inicio', 'hora_fin', 'tiempo_planeado', 'produccion_planeada']
