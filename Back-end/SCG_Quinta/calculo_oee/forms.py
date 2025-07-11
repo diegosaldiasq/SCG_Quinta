217e62be79cfe8e27c52a1fbc6af0bdced04f43e
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2025-07-11 15:58:11 -0400

    se hace configuracion final de fincionamiento

--- Back-end/SCG_Quinta/calculo_oee/forms.py
+++ Back-end/SCG_Quinta/calculo_oee/forms.py
@@ -1,10 +1,6 @@
 from django import forms
 from .models import TurnoOEE
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b77b85f6b743719e389a9c0e3bed7fc4b5ee741a
 class TurnoOEEForm(forms.ModelForm):
     fecha = forms.DateField(
         widget=forms.DateInput(attrs={
@@ -77,15 +73,4 @@ class TurnoOEEForm(forms.ModelForm):
 class ProduccionRealForm(forms.ModelForm):
     class Meta:
         model = TurnoOEE
-<<<<<<< HEAD
         fields = ['produccion_real']
-=======
-        fields = ['produccion_real']
-        widget = {
-            'produccion_real': forms.NumberInput(attrs={
-                'readonly': 'readonly',
-                'id': 'id_produccion_real',
-                'placeholder': 'Ingrese producción real'
-            })
-        }
->>>>>>> b77b85f6b743719e389a9c0e3bed7fc4b5ee741a
