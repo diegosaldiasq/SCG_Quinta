4294fa8609b456170af626d6dbfd9ea5b1767d63
Diego Saldias <diego.saldias.quijada@gmail.com>
2025-07-20 10:30:54 -0400

    se corrige error de urls

--- Back-end/SCG_Quinta/calculo_oee/urls.py
+++ Back-end/SCG_Quinta/calculo_oee/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     #path("", views.calculo_oee, name="calculo_oee"),
     path('crear_turno/', views.crear_turno, name='crear_turno'),
     path('turno-exito/', lambda request: HttpResponse("Turno registrado con éxito."), name='turno_exito'),
-    path('resumen/<int:turno_id>/', views.resumen_turno, name='resumen_turno'),
-    path('cerrar_turno/<int:turno_id>/', views.cerrar_turno, name='cerrar_turno'),
+    path('resumen/<int:lote_id>/', views.resumen_turno, name='resumen_turno'),
+    path('cerrar_turno/<int:lote_id>/', views.cerrar_turno, name='cerrar_turno'),
     path('lista_turnos/', views.lista_turnos, name='lista_turnos'),
-    path('detalle_turno/<int:turno_id>/', views.detalle_turno, name='detalle_turno'),
-    path('marcar_verificado/<int:turno_id>/', views.marcar_verificado, name='marcar_verificado'),
+    path('detalle_turno/<int:lote_id>/', views.detalle_turno, name='detalle_turno'),
+    path('marcar_verificado/<int:lote_id>/', views.marcar_verificado, name='marcar_verificado'),
     path('redireccionar_intermedio/', views.redireccionar_intermedio, name='redireccionar_intermedio'),
     path('descargar_resumenturnooee/', views.descargar_resumenturnooee, name='descargar_resumenturnooee')
 ]
