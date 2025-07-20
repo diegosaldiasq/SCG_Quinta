4db9f5da15a18a339e1a94d031d6b83509e4c7eb
Diego Saldias <diego.saldias.quijada@gmail.com>
2025-07-20 03:44:52 -0400

    se intentanuevamente otra consulta a resumen turno oee 2

--- Back-end/SCG_Quinta/inicio/views.py
+++ Back-end/SCG_Quinta/inicio/views.py
@@ -146,7 +146,7 @@ model_mapping = {
         'control_parametros_gorreri': 'DatosFormularioControlParametrosGorreri',
         'control_de_pesos_prelistos': 'DatosFormularioControlDePesosPrelistos',
         'control_de_pesos_insumos_kuchen': 'DatosFormularioControlDePesosInsumosKuchen',
-        'calculo_oee': 'calculo_oee.models.ResumenTurnoOee',
+        'calculo_oee': 'calculo_oee_ResumenTurnoOee'
     }
 
 @csrf_exempt
