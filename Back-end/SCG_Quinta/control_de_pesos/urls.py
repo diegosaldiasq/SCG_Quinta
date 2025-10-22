bea96720e029696c974331c282d4aa88d16acd6d
Diego Saldias <diego.saldias.quijada@gmail.com>
2025-10-22 18:28:40 -0300

    se cambia boton de salida y estilo de botones

--- Back-end/SCG_Quinta/control_de_pesos/urls.py
+++ Back-end/SCG_Quinta/control_de_pesos/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("selecciones/", views.redireccionar_selecciones_2, name="redireccionar_selecciones_2"),
     path('graficos/', views.graficos_control_pesos, name='graficos_control_pesos'),
     path('api/graficos_control_pesos/', views.api_graficos_control_pesos, name='api_graficos_control_pesos'),
-    path('api/productos_por_cliente/', views.api_productos_por_cliente, name='api_productos_por_cliente')
+    path('api/productos_por_cliente/', views.api_productos_por_cliente, name='api_productos_por_cliente'),
+    path('redireccionar_intermedio_4/', views.redireccionar_intermedio_4, name='redireccionar_intermedio_4')
 ]
