b33062491519f7b95c205c972b3cf9e8115d1b25
Diego Saldias <dsaldias@QUINTA.LOCAL>
2025-10-27 12:50:08 -0300

    se agrega grafico control pesos prelistos

--- Back-end/SCG_Quinta/control_de_pesos_prelistos/urls.py
+++ Back-end/SCG_Quinta/control_de_pesos_prelistos/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("", views.control_de_pesos_prelistos, name="control_de_pesos_prelistos"),
     path("vista_control_de_pesos_prelistos/", views.vista_control_de_pesos_prelistos, name="vista_control_de_pesos_prelistos"),
     path("selecciones/", views.redireccionar_selecciones_2, name="redireccionar_selecciones_2"),
-    path('prelistos/graficos/', views.graficos_control_pesos_prelistos, name='graficos_control_pesos_prelistos'),
-    path('prelistos/api/productos/', views.api_productos_por_cliente_prelistos, name='api_productos_por_cliente_prelistos'),
-    path('prelistos/api/datos/', views.api_graficos_control_pesos_prelistos, name='api_graficos_control_pesos_prelistos'),
+    path('graficos/', views.graficos_control_pesos_prelistos, name='graficos_control_pesos_prelistos'),
+    path('api/productos/', views.api_productos_por_cliente_prelistos, name='api_productos_por_cliente_prelistos'),
+    path('api/datos/', views.api_graficos_control_pesos_prelistos, name='api_graficos_control_pesos_prelistos'),
     path('redireccionar_intermedio_4/', views.redireccionar_intermedio_4, name='redireccionar_intermedio_4')
 ]
