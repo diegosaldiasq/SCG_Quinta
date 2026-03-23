9ee385ee81107f1caa4a036a11f14a5da3d4c477
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2026-03-23 12:40:56 -0300

    se prueba con urls completos

--- Back-end/SCG_Quinta/ventas_geo/urls.py
+++ Back-end/SCG_Quinta/ventas_geo/urls.py
@@ -5,6 +5,6 @@ app_name = 'ventas_geo'
 
 urlpatterns = [
     path('cargar/', views.cargar_ventas, name='cargar_ventas'),
-    #path('resultado/<int:carga_id>/', views.resultado_carga, name='resultado_carga'),
-    #path('dashboard/', views.dashboard_ventas_geo, name='dashboard'),
+    path('resultado/<int:carga_id>/', views.resultado_carga, name='resultado_carga'),
+    path('dashboard/', views.dashboard_ventas_geo, name='dashboard'),
 ]
