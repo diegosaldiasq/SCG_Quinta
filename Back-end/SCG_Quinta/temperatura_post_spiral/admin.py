67c3e2ca979a21d4d9d5bd74316c221e81b5cd7e
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2026-05-04 10:09:57 -0400

    se corrige admin de temperatura post spiral

--- Back-end/SCG_Quinta/temperatura_post_spiral/admin.py
+++ Back-end/SCG_Quinta/temperatura_post_spiral/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RegistroTemperaturaPostSpiral
+from .models import RegistroTemperaturaPostSpiral, DetalleTemperaturaPostSpiral
 
 
 @admin.register(RegistroTemperaturaPostSpiral)
@@ -12,7 +12,7 @@ class RegistroTemperaturaPostSpiralAdmin(admin.ModelAdmin):
         'producto',
         'codigo',
         'lote',
-        'temperatura',
+        'cantidad_temperaturas',
         'acciones_correctivas_verificadas',
         'verificado',
     ]
@@ -43,4 +43,26 @@ class RegistroTemperaturaPostSpiralAdmin(admin.ModelAdmin):
         'nombre_verificador',
         'fecha_revision_accion_correctiva',
         'nombre_revisor_accion_correctiva',
+    ]
+
+@admin.register(DetalleTemperaturaPostSpiral)
+class DetalleTemperaturaPostSpiralAdmin(admin.ModelAdmin):
+    list_display = [
+        'registro',
+        'numero',
+        'temperatura',
+        'accion_correctiva',
+        'creado_en',
+    ]
+
+    search_fields = [
+        'registro__cliente',
+        'registro__producto',
+        'registro__codigo',
+        'registro__lote',
+    ]
+
+    list_filter = [
+        'registro__turno',
+        'registro__cliente',
     ]
