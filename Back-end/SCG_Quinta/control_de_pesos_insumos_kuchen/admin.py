9e618d67322298b9b4a7a4243d665102e6415c88
Diego Saldias Quijada <diego.saldias.quijada@gmail.com>
2026-05-08 13:16:51 -0400

    se agrega admin para prelistos e insumos kuchen

--- Back-end/SCG_Quinta/control_de_pesos_insumos_kuchen/admin.py
+++ Back-end/SCG_Quinta/control_de_pesos_insumos_kuchen/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    DatosFormularioControlDePesosInsumosKuchen
+)
+
+
+@admin.register(DatosFormularioControlDePesosInsumosKuchen)
+class DatosFormularioControlDePesosInsumosKuchenAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "fecha_registro",
+        "nombre_tecnologo",
+        "cliente",
+        "codigo_producto",
+        "producto",
+        "peso_receta",
+        "peso_real",
+        "altura",
+        "desviacion_porcentaje",
+        "lote",
+        "turno",
+        "verificado",
+    )
+
+    list_filter = (
+        "cliente",
+        "turno",
+        "verificado",
+        "fecha_registro",
+    )
+
+    search_fields = (
+        "cliente",
+        "codigo_producto",
+        "producto",
+        "lote",
+        "nombre_tecnologo",
+    )
+
+    readonly_fields = (
+        "fecha_registro",
+        "desviacion_porcentaje",
+    )
+
+    ordering = (
+        "-fecha_registro",
+    )
+
+    list_per_page = 50
